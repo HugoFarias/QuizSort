0d4c1f9fc68da48685e3e13d8b1ef7c479ddca80
Hugo Farias <hugofarias@live.jp>
2014-12-19 11:38:42 -0200

    basic struct of program and ignore test files

--- quiz.c
+++ quiz.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char const *argv[])
-{
-	printf("%s/n", *argv);
-	//FILE* file = fopen();
+int main(int argc, char **argv){
+	FILE* file = fopen(argv[1], "r");
+	if(!file) return NULL;
+	printf("%s\b%s\n", argv[1], "aberto!");
+
+	char* cmd;
+	while(1){ //stricmp() compara strings considerando A == a. 0 se iguais.
+		scanf("%s", cmd);
+		printf("\n");
+
+		if (!stricmp(cmd, "exit")) break;
+		else if(!stricmp(cmd, "help")) printf("Insira o comando desejado:\nAjuda: help\nGerar pergunta: ans\nSair: exit");
+		else if (!stricmp(cmd, "ans")) printf("%s\n", genAns());
+	}
+
+	fclose(file);
 	return 0;
+}
+
+char* genAns(){
+	return "Funcionando\n";
 }
