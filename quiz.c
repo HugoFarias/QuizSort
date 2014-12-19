1e5d4e8c1f9e210537a5a93ad73bae64b0f9a19c
Hugo Farias <hugofarias@live.jp>
2014-12-19 12:16:45 -0200

    text interface ok

--- quiz.c
+++ quiz.c
@@ -1,25 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+char* genAns(){
+	
+	return "funcionando";
+}
 
 int main(int argc, char **argv){
 	FILE* file = fopen(argv[1], "r");
-	if(!file) return NULL;
-	printf("%s\b%s\n", argv[1], "aberto!");
+	if(!(long)file) return 1;
+	printf("%s%s\n\n", argv[1], " aberto!\nPara ajuda digite: help");
 
-	char* cmd;
-	while(1){ //stricmp() compara strings considerando A == a. 0 se iguais.
+	int cnt = 1;
+	char *cmd = malloc(sizeof(char)*4);
+	while(1){
 		scanf("%s", cmd);
 		printf("\n");
 
-		if (!stricmp(cmd, "exit")) break;
-		else if(!stricmp(cmd, "help")) printf("Insira o comando desejado:\nAjuda: help\nGerar pergunta: ans\nSair: exit");
-		else if (!stricmp(cmd, "ans")) printf("%s\n", genAns());
+		if (!strcmp(cmd, "exit")){
+			break;
+		}else{
+			if(!strcmp(cmd, "help")){
+				printf("Ajuda: help\nGerar pergunta: ans\nSair: exit\n\n");
+			}else{
+				if (!strcmp(cmd, "ans")){
+					printf("PERGUNTA (%d): %s\n\n", cnt, genAns());
+					cnt++;
+				}else{
+					continue;
+				}
+			}
+		}
 	}
 
+	free(cmd);
 	fclose(file);
 	return 0;
-}
-
-char* genAns(){
-	return "Funcionando\n";
 }
